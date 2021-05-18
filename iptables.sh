31b38723ee3e22c7f644a79e2ec099af27171458
Aaron Akhtar <52239843+Aaron-Akhtar@users.noreply.github.com>
2021-05-18 08:33:47 +0100

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,3 +1,2 @@
 # "%port%" - replace with the port your instance of SideD00r is running, by default the port is 900.
-# "%path%" - replace with your SideD00r Jar File Path.
-iptables -I INPUT -p tcp --dport %port% -j ACCEPT; screen -dm java -jar %path%
+iptables -I INPUT -p tcp --dport %port% -j ACCEPT;
